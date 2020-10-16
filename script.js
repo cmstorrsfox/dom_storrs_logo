64ae909ec99578d077889a12549631b425414d62
Chris Storrs-Fox <cmstorrsfox@gmail.com>
2020-10-16 21:38:08 +0100

    added divs for loudness

--- script.js
+++ script.js
@@ -1,90 +1,94 @@
 const dialOne = document.getElementById('dial-1');
 const dialTwo = document.getElementById('dial-2');
 const logoContainer = document.getElementById('logo-container');
-const coordDisplay = document.getElementById('display-coords');
+const turnZoneOneFourRight = document.getElementById('one-4-r');
+const turnZoneOneTwoRight = document.getElementById('one-2-r');
+const turnZoneOneFourLeft = document.getElementById('one-4-l');
+const turnZoneOneTwoLeft = document.getElementById('one-2-l');
+const turnZoneTwoFourRight = document.getElementById('two-4-r');
+const turnZoneTwoTwoRight = document.getElementById('two-2-r');
+const turnZoneTwoFourLeft = document.getElementById('two-4-l');
+const turnZoneTwoTwoLeft = document.getElementById('two-2-l');
 
-//function to log position
-function logPos(event) {
-  const rect = event.currentTarget.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-  console.log(rect)
-  coordDisplay.innerHTML = `X = ${x}, Y = ${y}`
-}
-
-logoContainer.addEventListener('click', logPos);
 
 //dial 1 functions and event listeners
-function rotateDialOneRight(event) {
-  const rect = event.currentTarget.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-
-  if(x > 360 && x < 490 && y < 220)  {
-
-    dialOne.style.transition = "all 0.7s ease";
-    dialOne.style.transform = "rotate(90deg)";
-    console.log(`dial right running at ${x} and ${y}`)
-  } 
+//dial 1 two-clicks right
+function rotateDialOneRightTwoClicks() {
+  dialOne.style.transition = "all 0.7s ease";
+  dialOne.style.transform = "rotate(45deg)";
 }
 
-function rotateDialOneLeft(event) {
-  const rect = event.currentTarget.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
+//dial 1 two-clicks left
+function rotateDialOneLeftTwoClicks() {
+  dialOne.style.transition = "all 0.7s ease";
+  dialOne.style.transform = "rotate(-45deg)";
 
-  if(x > 174 && x < 320 && y < 220)  {
-    dialOne.style.transition = "all 0.7s ease";
-    dialOne.style.transform = "rotate(-90deg)";
-    console.log(`dial left running at ${x} and ${y}`)
-
-  }
 }
+
+//dial 1 four-clicks right
+function rotateDialOneRightFourClicks() {
+  dialOne.style.transition = "all 0.7s ease";
+  dialOne.style.transform = "rotate(90deg)";
+}
+
+//dial 1 four-clicks left
+function rotateDialOneLeftFourClicks() {
+  dialOne.style.transition = "all 0.7s ease";
+  dialOne.style.transform = "rotate(-90deg)";
+
+}
+
 
 //return dial to start
 function returnDialOne() {
   dialOne.style.transform = "rotate(0)";
 }
 
-
-logoContainer.addEventListener('mousemove', rotateDialOneRight);
-logoContainer.addEventListener('mousemove', rotateDialOneLeft);
+/*
+logoContainer.addEventListener('mouseover', rotateDialOneRightTwoClicks);
+logoContainer.addEventListener('click', rotateDialOneLeftTwoClicks);
+dialOne.addEventListener('mouseenter', rotateDialOneRightFourClicks);
+dialOne.addEventListener('dblclick', rotateDialOneLeftFourClicks);
 logoContainer.addEventListener('mouseleave', returnDialOne);
-
+*/
 
 //dial 2 functions and event listeners
-function rotateDialTwoRight(event) {
-  const rect = event.currentTarget.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-
-  if(x > 688 && x < 815 && y > 120 && y < 320)  {
-
-    dialTwo.style.transition = "all 0.7s ease";
-    dialTwo.style.transform = "rotate(90deg)";
-    console.log(`dial right running at ${x} and ${y}`)
-  } 
+//dial 2 two-clicks right
+function rotateDialTwoRightTwoClicks() {
+  dialTwo.style.transition = "all 0.7s ease";
+  dialTwo.style.transform = "rotate(45deg)";
 }
 
-function rotateDialTwoLeft(event) {
-  const rect = event.target.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
+//dial 2 two-clicks left
+function rotateDialTwoLeftTwoClicks() {
+  dialTwo.style.transition = "all 0.7s ease";
+  dialTwo.style.transform = "rotate(-45deg)";
 
-  if(x > 450 && x < 655 && y > 120 && y < 320)  {
-    dialTwo.style.transition = "all 0.7s ease";
-    dialTwo.style.transform = "rotate(-90deg)";
-    console.log(`dial left running at ${x} and ${y}`)
-
-  }
 }
+
+//dial 2 four-clicks right
+function rotateDialTwoRightFourClicks() {
+  dialTwo.style.transition = "all 0.7s ease";
+  dialTwo.style.transform = "rotate(90deg)";
+}
+
+//dial 2 four-clicks left
+function rotateDialTwoLeftFourClicks() {
+  dialTwo.style.transition = "all 0.7s ease";
+  dialTwo.style.transform = "rotate(-90deg)";
+
+}
+
 
 //return dial to start
 function returnDialTwo() {
   dialTwo.style.transform = "rotate(0)";
 }
 
-
-logoContainer.addEventListener('mousemove', rotateDialTwoRight);
-logoContainer.addEventListener('mousemove', rotateDialTwoLeft);
+/*
+logoContainer.addEventListener('mouseover', rotateDialTwoRightTwoClicks);
+logoContainer.addEventListener('click', rotateDialTwoLeftTwoClicks);
+dialTwo.addEventListener('mouseenter', rotateDialTwoRightFourClicks);
+dialTwo.addEventListener('dblclick', rotateDialTwoLeftFourClicks);
 logoContainer.addEventListener('mouseleave', returnDialTwo);
+*/
