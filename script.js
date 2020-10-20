cdcbc6ed0e36fe6b3f402eee5c770870f0600a4f
Chris Storrs-Fox <cmstorrsfox@gmail.com>
2020-10-20 17:12:26 +0100

    working with mouse move

--- script.js
+++ script.js
@@ -1,133 +1,109 @@
 const dialOne = document.getElementById('dial-1');
 const dialTwo = document.getElementById('dial-2');
 const logoContainer = document.getElementById('logo-container');
-const turnZoneOneFourRight = document.getElementById('one-4-r');
-const turnZoneOneTwoRight = document.getElementById('one-2-r');
-const turnZoneOneFourLeft = document.getElementById('one-4-l');
-const turnZoneOneTwoLeft = document.getElementById('one-2-l');
-const turnZoneTwoFourRight = document.getElementById('two-4-r');
-const turnZoneTwoTwoRight = document.getElementById('two-2-r');
-const turnZoneTwoFourLeft = document.getElementById('two-4-l');
-const turnZoneTwoTwoLeft = document.getElementById('two-2-l');
 const trackOne = document.getElementById('track-1');
 const trackTwo = document.getElementById('track-2');
 
 
 //dial 1 functions and event listeners
-//dial 1 two-clicks right
-function rotateDialOneRightTwoClicks() {
-  dialOne.style.transition = "all 0.7s ease";
-  dialOne.style.transform = "rotate(45deg)";
-  trackOne.play();
-  trackOne.muted = false;
-  trackOne.volume = 0.75;
+//dial 1 
+function rotateDialOne(event) {
+  const x = event.clientX;
+  const y = event.clientY;
+  console.log(`x = ${x} and y = ${y}`);
+
+  if(y < 870 && y > 750 && x < 600) {
+    dialOne.style.transition = "all 0.5s linear";
+    dialOne.style.transform = "rotate(-90deg)";
+    trackOne.play();
+    trackOne.muted = false;
+    trackOne.volume = 0.1;
+  } else if (y < 750 && y > 540 && x < 600) {
+    dialOne.style.transition = "all 0.75s linear";
+    dialOne.style.transform = "rotate(-45deg)";
+    trackOne.play();
+    trackOne.muted = false;
+    trackOne.volume = 0.3;
+  } else if (y < 540 && y > 340 && x < 600) {
+    dialOne.style.transition = "all 0.75s linear";
+    dialOne.style.transform = "rotate(0deg)";
+    trackOne.play();
+    trackOne.muted = false;
+    trackOne.volume = 0.5;
+  } else if (y < 340 && y > 140 && x < 600) {
+    dialOne.style.transition = "all 0.75s linear";
+    dialOne.style.transform = "rotate(45deg)";
+    trackOne.play();
+    trackOne.muted = false;
+    trackOne.volume = 0.7;
+  } else if(y < 140 && y > 50 && x < 600) {
+    dialOne.style.transition = "all 0.75s linear";
+    dialOne.style.transform = "rotate(90deg)";
+    trackOne.play();
+    trackOne.muted = false;
+    trackOne.volume = 0.9;
+  } else if (y < 50 && x < 600) {
+    dialOne.style.transition = "all 0.75s linear";
+    dialOne.style.transform = "rotate(120deg)";
+    trackOne.play();
+    trackOne.muted = false;
+    trackOne.volume = 1;
+  } else {
+    dialOne.style.transform = "rotate(-120deg)";
+    trackOne.muted = true;
+  }
 }
 
-//dial 1 two-clicks left
-function rotateDialOneLeftTwoClicks() {
-  dialOne.style.transition = "all 0.7s ease";
-  dialOne.style.transform = "rotate(-45deg)";
-  trackOne.play();
-  trackOne.muted = false;
-  trackOne.volume = 0.25;
+document.addEventListener('mousemove', rotateDialOne);
 
-}
-
-//dial 1 four-clicks right
-function rotateDialOneRightFourClicks() {
-  dialOne.style.transition = "all 0.7s ease";
-  dialOne.style.transform = "rotate(90deg)";
-  trackOne.play();
-  trackOne.muted = false;
-  trackOne.volume = 1;
-}
-
-//dial 1 four-clicks left
-function rotateDialOneLeftFourClicks() {
-  dialOne.style.transition = "all 0.7s ease";
-  dialOne.style.transform = "rotate(-90deg)";
-  trackOne.play();
-  trackOne.muted = false;
-  trackOne.volume = 0.1;
-
-}
-
-
-//return dial to start
-function returnDialOne() {
-  dialOne.style.transform = "rotate(0)";
-  trackOne.pause();
-}
-
-//event listeners for dial 1
-//start animation
-turnZoneOneTwoRight.addEventListener('mouseenter', rotateDialOneRightTwoClicks);
-turnZoneOneTwoLeft.addEventListener('mouseenter', rotateDialOneLeftTwoClicks);
-turnZoneOneFourRight.addEventListener('mouseenter', rotateDialOneRightFourClicks);
-turnZoneOneFourLeft.addEventListener('mouseenter', rotateDialOneLeftFourClicks);
-
-//stop animation
-turnZoneOneTwoRight.addEventListener('mouseleave', returnDialOne);
-turnZoneOneTwoLeft.addEventListener('mouseleave', returnDialOne);
-turnZoneOneFourRight.addEventListener('mouseleave', returnDialOne);
-turnZoneOneFourLeft.addEventListener('mouseleave', returnDialOne);
 
 //dial 2 functions and event listeners
-//dial 2 two-clicks right
-function rotateDialTwoRightTwoClicks() {
-  dialTwo.style.transition = "all 0.7s ease";
-  dialTwo.style.transform = "rotate(45deg)";
-  trackTwo.play();
-  trackTwo.muted = false;
-  trackTwo.volume = 0.75;
+//dial 2 
+function rotateDialTwo(event) {
+  const x = event.clientX;
+  const y = event.clientY;
+  console.log(`x = ${x} and y = ${y}`);
+
+  if(y < 870 && y > 750 && x > 600) {
+    dialTwo.style.transition = "all 0.5s linear";
+    dialTwo.style.transform = "rotate(-90deg)";
+    trackTwo.play();
+    trackTwo.muted = false;
+    trackTwo.volume = 0.1;
+  } else if (y < 750 && y > 540 && x > 600) {
+    dialTwo.style.transition = "all 0.75s linear";
+    dialTwo.style.transform = "rotate(-45deg)";
+    trackTwo.play();
+    trackTwo.muted = false;
+    trackTwo.volume = 0.3;
+  } else if (y < 540 && y > 340 && x > 600) {
+    dialTwo.style.transition = "all 0.75s linear";
+    dialTwo.style.transform = "rotate(0deg)";
+    trackTwo.play();
+    trackTwo.muted = false;
+    trackTwo.volume = 0.5;
+  } else if (y < 340 && y > 140 && x > 600) {
+    dialTwo.style.transition = "all 0.75s linear";
+    dialTwo.style.transform = "rotate(45deg)";
+    trackTwo.play();
+    trackTwo.muted = false;
+    trackTwo.volume = 0.7;
+  } else if(y < 140 && y > 50 && x > 600) {
+    dialTwo.style.transition = "all 0.75s linear";
+    dialTwo.style.transform = "rotate(90deg)";
+    trackTwo.play();
+    trackTwo.muted = false;
+    trackTwo.volume = 0.9;
+  } else if (y < 50 && x > 600) {
+    dialTwo.style.transition = "all 0.75s linear";
+    dialTwo.style.transform = "rotate(120deg)";
+    trackTwo.play();
+    trackTwo.muted = false;
+    trackTwo.volume = 1;
+  } else {
+    dialTwo.style.transform = "rotate(-120deg)";
+    trackTwo.muted = true;
+  }
 }
 
-//dial 2 two-clicks left
-function rotateDialTwoLeftTwoClicks() {
-  dialTwo.style.transition = "all 0.7s ease";
-  dialTwo.style.transform = "rotate(-45deg)";
-  trackTwo.play();
-  trackTwo.muted = false;
-  trackTwo.volume = 0.25;
-
-
-}
-
-//dial 2 four-clicks right
-function rotateDialTwoRightFourClicks() {
-  dialTwo.style.transition = "all 0.7s ease";
-  dialTwo.style.transform = "rotate(90deg)";
-  trackTwo.play();
-  trackTwo.muted = false;
-  trackTwo.volume = 1;
-}
-
-//dial 2 four-clicks left
-function rotateDialTwoLeftFourClicks() {
-  dialTwo.style.transition = "all 0.7s ease";
-  dialTwo.style.transform = "rotate(-90deg)";
-  trackTwo.play();
-  trackTwo.muted = false;
-  trackTwo.volume = 0.1;
-
-}
-
-
-//return dial to start
-function returnDialTwo() {
-  dialTwo.style.transform = "rotate(0)";
-  trackTwo.pause();
-}
-
-//start animation
-turnZoneTwoTwoRight.addEventListener('mouseenter', rotateDialTwoRightTwoClicks);
-turnZoneTwoTwoLeft.addEventListener('mouseenter', rotateDialTwoLeftTwoClicks);
-turnZoneTwoFourRight.addEventListener('mouseenter', rotateDialTwoRightFourClicks);
-turnZoneTwoFourLeft.addEventListener('mouseenter', rotateDialTwoLeftFourClicks);
-
-//stop animation
-turnZoneTwoTwoRight.addEventListener('mouseleave', returnDialTwo);
-turnZoneTwoTwoLeft.addEventListener('mouseleave', returnDialTwo);
-turnZoneTwoFourRight.addEventListener('mouseleave', returnDialTwo);
-turnZoneTwoFourLeft.addEventListener('mouseleave', returnDialTwo);
-
+document.addEventListener('mousemove', rotateDialTwo);
